2742bc7a0274177876fb886b55ed26d95879f0f2
EmmanFring <nwaikechukwu50@gmail.com>
2025-02-05 15:04:51 +0100

    Updated  config file

--- database/config.js
+++ database/config.js
@@ -1,10 +1,15 @@
 // // this file is for us to make configuration and   connection to the database
-require("dotenv").config()
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const sequelizeConfig = new Sequelize(process.env.databased, process.env.usernamed, process.env.passwordd, {
-  host: process.env.hostd,
-  dialect: "mysql",
-  logging: false,
-});
+const sequelizeConfig = new Sequelize(
+  process.env.databasep,
+  process.env.usernamep,
+  process.env.passwordp,
+  {
+    host: process.env.hostp,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 module.exports = { sequelizeConfig };
